4492014a9d1cb05dd9ded9c33db2730e663733f5
Vansh Dobariya <167301367+VanshBD@users.noreply.github.com>
2024-10-05 15:21:46 +0530

    fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,6 +24,9 @@ dbConnection();
 // api routes s
 app.use("/api/users", userRoutes);
 app.use("/api/recipes", recipeRoutes);
+app.get('/',(req,res)=>{
+  res.status(200).send('server started ')
+})
 
 app.listen(PORT, (err) => {
   if (err) {
